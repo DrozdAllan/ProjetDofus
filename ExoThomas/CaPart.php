25a178908bd410e004160376e9e86cec5bede0ef
DrozdAllan <drozdallan@gmail.com>
2020-12-12 03:01:20 +0100

    avance sur POO Zobal

--- ExoThomas/CaPart.php
+++ ExoThomas/CaPart.php
@@ -10,72 +10,110 @@ class Personnage
         $this->Serveur = $Serveur;
         $this->Niveau = $Niveau;
         $this->Stats = $Stats;
-        // $Stats = {$Vita, $For, $Agi, $Intel, $Sa};
+        // $Stats = {$Vita, $For, $Agi, $Intel, $Sa, $Pa, $Pm, $Po};
 
     }
 
-    public function setPseudo($Pseudo) {
+    public function setPseudo($Pseudo)
+    {
         if ($Pseudo != ' ') {
 
             $this->Pseudo = $Pseudo;
+        } else {
+            throw new Exception("Le pseudo ne peux pas être vide");
         }
-    else {
-        throw new Exception("Le pseudo ne peux pas être vide");
-    }
     }
 
-    public function getPseudo() {
+    public function getPseudo()
+    {
         return $this->Pseudo;
+    }
+
+    public function move()
+    {
+        if ($this->Pm <= 0) {
+            echo $this->Pseudo . ' n\' a pas assez de Pm pour se déplacer ';
+        } else {
+            echo $this->Pseudo . ' se deplace';
+        }
+    }
+
+
+    public function receivedmg()
+    {
+        if ($this->Vitalite - $this->DegatSort <= 0) {
+            echo $this->Pseudo . ' a succombe a ses blessures';
+        } else {
+            echo $this->Pseudo . ' recoit des degats';
+        }
     }
 
     public function talk()
     {
         echo 'salut je m\'appelle ' . $this->Pseudo . ', je suis niveau ' . $this->Niveau . ' et je joue sur le serveur ' . $this->Serveur;
     }
-
-    public function move()
-    {
-        echo $this->Pseudo . ' se deplace';
-    }
-
-
-    public function receivedmg()
-    {
-        echo $this->Pseudo . ' recoit des degats';
-    }
-
-    public function die()
-    {
-        if ($this->Vitalite <= 0) {
-            echo $this->Pseudo . ' a succombe a ses blessures';
-        } else {
-            echo $this->Pseudo . ' n\'est pas encore mort !';
-        }
-    }
 }
 
-$Tchory = new Personnage('Tchory', 'Jahash', 136, 23);
-$Tchory->talk();
+
+
+
+
+
+
 
 
 
 class Zobal extends Personnage
 {
-    public $Sorts;
     
-    public function __construct($Pseudo, $Serveur, $Niveau, $Stats, $Sorts)
-    {
-    parent::__construct($Pseudo, $Serveur, $Niveau, $Stats);
     
-    $this->Sorts = $Sorts;    
-}
-
-
-
-
-
-
-    public function degatsdusort()
+    public function __construct($Pseudo, $Serveur, $Niveau, $Stats, $NomSort, $DegatSort, $ElemSort, $CibleSort)
     {
+        parent::__construct($Pseudo, $Serveur, $Niveau, $Stats);
+        
+        $this->NomSort = $NomSort;
+        $this->DegatSort = $DegatSort;
+        $this->ElemSort = $ElemSort;
+        $this->CibleSort = $CibleSort;
+        
+    }   
+    
+    public function setNomSort() {
+        $this->NomSort = $NomSort; // Pk erreur undefined variable ?
     }
-}
+    
+    public function getNomSort()
+    {
+        return $this->NomSort;
+    }
+    
+    
+    public function setDegatSort() {
+        if (is_int($this->DegatSort))
+        {
+            $this->DegatSort = $DegatSort;
+        }
+    }
+    
+    public function getDegatSort() {
+        return $this->DegatSort;
+    }
+    
+    public function setElemSort() {
+        if (is_string($this->ElemSort))
+        {
+            $this->ElemSort = $ElemSort;
+        }
+    }
+    
+    public function boliche() {
+            $ElemSort = 'Eau';
+            $DegatSort = 18;
+        }
+    }
+    
+   // $Tchory = new Personnage('Tchory', 'Jahash', 136, 23);
+   $Tchory = new Zobal('Tchory', 'Jahash', 136, 23); 
+   $Tchory->talk();
+
+   // Comment faire marcher un sort ? Pas compris 
